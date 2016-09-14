519628b177853966c940a3fa29064d20b2d7d0fe
Patrick Armstrong <parmstrong@groupcse.com>
2016-09-13 21:42:32 -0400

    Style improvements, cat pics, git ignore

--- index.php
+++ index.php
@@ -16,10 +16,32 @@
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/>
 	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 	<script   src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+	<style type="text/css">
+		h1 {
+			margin: auto;
+			font-size: 50px;
+		}
+		h2 {
+			position: relative;
+			top: 200px;
+			left: 100px;
+			pointer-events: none;
+			color: #FFFFFF;
+			-webkit-text-stroke-width: 1px;
+   			-webkit-text-stroke-color: black;
+		}
+	</style>
 
 	<script type="text/javascript">
+	var sourceSwap = function () {
+	    var $this = $(this);
+	    var newSource = $this.data('alt-src');
+	    $this.data('alt-src', $this.attr('src'));
+	    $this.attr('src', newSource);
+	}
+
 	$(document).ready(function() {
-		var $links = $('h2 a.active');
+		var $links = $('a.active');
 
 		$links.on('click', function(e) {
 			e.preventDefault();
@@ -32,9 +54,13 @@
 				updateCount($(this), url);
 			});
 		}
-		window.setInterval(function() { 
-			checkForUpdates(); 
-		}, 5000);
+		// window.setInterval(function() { 
+		// 	checkForUpdates(); 
+		// }, 5000);
+
+		$('img[data-alt-src]').each(function() { 
+	        new Image().src = $(this).data('alt-src'); 
+	    }).hover(sourceSwap, sourceSwap); 
 	});
 
 	function updateCount($ptr, url) {
@@ -75,10 +101,12 @@
 				<h1>Bae counter</h1>
 			</div>
 			<div class="span6">
-				<h2><a class="active" id="patrick" href="?counter=patrick">Patrick: </a><span><?php echo $counters[patrick]; ?></span></h2>
+				<h2>Patrick: <span id="patrick-count"><?php echo $counters[patrick]; ?></span></h2>
+				<a class="active" id="patrick" href="?counter=patrick"><img src="cat-gif-1-1.gif" data-alt-src="cat-gif-1-2.gif" /></a>
 			</div>
 			<div class="span6">
-				<h2><a class="active" id="yingying" href="?counter=yingying">Yingying: </a><span><?php echo $counters[yingying]; ?></span></h2>
+				<h2>Yingying: <span><?php echo $counters[yingying]; ?></span></h2>
+				<a class="active" id="yingying" href="?counter=yingying"><img src="cat-gif-2-1.gif" data-alt-src="cat-gif-2-2.gif" /></a>
 			</div>
 		</div>
 	</div>
