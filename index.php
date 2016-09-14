8393aea98a088445cd2bf11e4f0742d0dfbb3c35
Patrick Armstrong <parmstrong@groupcse.com>
2016-09-14 14:17:27 -0400

    Added favicon

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
 		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/>
 		<link rel="stylesheet" type="text/css" href="screen.css"/>
+		<link id="favicon" rel="shortcut icon" href="img/favicon-32.png" sizes="16x16 32x32 48x48" type="image/png" />
 		<script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
 		<script src="main.js" type="text/javascript"></script>
 	</head>
