3f8e5bfff554998887c1fbacb8c8b4d851cfd0bb
Patrick Armstrong <parmstrong@groupcse.com>
2016-09-23 23:26:02 -0400

    Pitbull quiz

---
+++ what_fictional_character_are_you/index.php
@@ -0,0 +1,299 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>What Fictional Character Are You?</title>
+	<link type="text/css" rel="stylesheet" href="/what_fictional_character_are_you/style.min.css">
+</head>
+<body>
+	<form action="/what_fictional_character_are_you/results.php" method="post" name="qform">
+		<table id="userquiz" cellspacing="0"><tbody><tr><th class="first">1. What is your age?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q1" value="1" class="rad"> Under 18 Years Old</td>
+		</tr>
+		<tr><td><input type="radio" name="q1" value="2" class="rad"> 18 to 24 Years Old</td>
+		</tr>
+		<tr><td><input type="radio" name="q1" value="3" class="rad"> 25 to 30 Years Old</td>
+		</tr>
+		<tr><td><input type="radio" name="q1" value="4" class="rad"> 31 to 40 Years Old</td>
+		</tr>
+		<tr><td><input type="radio" name="q1" value="5" class="rad"> 41 to 50 Years Old</td>
+		</tr>
+		<tr><td><input type="radio" name="q1" value="6" class="rad"> 51 to 60 Years Old</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q1" value="7" class="rad"> Over 60 Years Old</td>
+		</tr>
+		<tr><th>2. What is your gender?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q2" value="1" class="rad"> Male</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q2" value="2" class="rad"> Female</td>
+		</tr>
+		<tr><th>3. You are physically strong.</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q3" value="1" class="rad"> Agree</td>
+		</tr>
+		<tr><td><input type="radio" name="q3" value="2" class="rad"> Maybe</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q3" value="3" class="rad"> Disagree</td>
+		</tr>
+		<tr><th>4. You are smart.</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q4" value="1" class="rad"> Agree</td>
+		</tr>
+		<tr><td><input type="radio" name="q4" value="2" class="rad"> Maybe</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q4" value="3" class="rad"> Disagree</td>
+		</tr>
+		<tr><th>5. Which sidekick would you choose?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q5" value="1" class="rad"> Dr. Watson</td>
+		</tr>
+		<tr><td><input type="radio" name="q5" value="2" class="rad"> Samwise Gamgee</td>
+		</tr>
+		<tr><td><input type="radio" name="q5" value="3" class="rad"> Robin (as in Batman and Robin)</td>
+		</tr>
+		<tr><td><input type="radio" name="q5" value="4" class="rad"> Snowy the dog</td>
+		</tr>
+		<tr><td><input type="radio" name="q5" value="5" class="rad"> Little John</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q5" value="6" class="rad"> Han Solo</td>
+		</tr>
+		<tr><th>6. Which weapon would you choose?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q6" value="1" class="rad"> A bullwhip</td>
+		</tr>
+		<tr><td><input type="radio" name="q6" value="2" class="rad"> My own two fists</td>
+		</tr>
+		<tr><td><input type="radio" name="q6" value="3" class="rad"> A longbow</td>
+		</tr>
+		<tr><td><input type="radio" name="q6" value="4" class="rad"> Handy gadgets</td>
+		</tr>
+		<tr><td><input type="radio" name="q6" value="5" class="rad"> Lightsaber (or any sword)</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q6" value="6" class="rad"> My brilliant mind. Oh...and a gun, just in case.</td>
+		</tr>
+		<tr><th>7. Favorite hat?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q7" value="1" class="rad"> One with a feather stuck in it at a jaunty angle</td>
+		</tr>
+		<tr><td><input type="radio" name="q7" value="2" class="rad"> Fedora</td>
+		</tr>
+		<tr><td><input type="radio" name="q7" value="3" class="rad"> The hood of a cloak, if that counts</td>
+		</tr>
+		<tr><td><input type="radio" name="q7" value="4" class="rad"> Deerstalker</td>
+		</tr>
+		<tr><td><input type="radio" name="q7" value="5" class="rad"> Well...just whatever sort of hats people are wearing these days</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q7" value="6" class="rad"> Sorry, no hat. It would get in the way of my mask.</td>
+		</tr>
+		<tr><th>8. Favorite of these characters?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q8" value="1" class="rad"> Lois Lane or Mary Jane Watson</td>
+		</tr>
+		<tr><td><input type="radio" name="q8" value="2" class="rad"> Any female characters from the Indiana Jones movies</td>
+		</tr>
+		<tr><td><input type="radio" name="q8" value="3" class="rad"> Maid Marian</td>
+		</tr>
+		<tr><td><input type="radio" name="q8" value="4" class="rad"> Princess Leia</td>
+		</tr>
+		<tr><td><input type="radio" name="q8" value="5" class="rad"> Mrs. Bailey</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q8" value="6" class="rad"> NONE.</td>
+		</tr>
+		<tr><th>9. Favorite animal of these?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q9" value="1" class="rad"> Eagle</td>
+		</tr>
+		<tr><td><input type="radio" name="q9" value="2" class="rad"> Fox</td>
+		</tr>
+		<tr><td><input type="radio" name="q9" value="3" class="rad"> Snake</td>
+		</tr>
+		<tr><td><input type="radio" name="q9" value="4" class="rad"> Spider</td>
+		</tr>
+		<tr><td><input type="radio" name="q9" value="5" class="rad"> Dog</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q9" value="6" class="rad"> Bat</td>
+		</tr>
+		<tr><th>10. Favorite genre?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q10" value="1" class="rad"> Action/Adventure</td>
+		</tr>
+		<tr><td><input type="radio" name="q10" value="2" class="rad"> Fantasy/sci-fi</td>
+		</tr>
+		<tr><td><input type="radio" name="q10" value="3" class="rad"> Mystery</td>
+		</tr>
+		<tr><td><input type="radio" name="q10" value="4" class="rad"> Animated</td>
+		</tr>
+		<tr><td><input type="radio" name="q10" value="5" class="rad"> Superhero</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q10" value="6" class="rad"> Classic</td>
+		</tr>
+		<tr><th>11. Your preferred outfit?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q11" value="1" class="rad"> Red and blue</td>
+		</tr>
+		<tr><td><input type="radio" name="q11" value="2" class="rad"> A cape-backed coat</td>
+		</tr>
+		<tr><td><input type="radio" name="q11" value="3" class="rad"> Anything that's practical</td>
+		</tr>
+		<tr><td><input type="radio" name="q11" value="4" class="rad"> Anything that's comfortable</td>
+		</tr>
+		<tr><td><input type="radio" name="q11" value="5" class="rad"> Anything dark</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q11" value="6" class="rad"> Lincoln green</td>
+		</tr>
+		<tr><th>12. Favorite name?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q12" value="1" class="rad"> Peter</td>
+		</tr>
+		<tr><td><input type="radio" name="q12" value="2" class="rad"> Clark</td>
+		</tr>
+		<tr><td><input type="radio" name="q12" value="3" class="rad"> George</td>
+		</tr>
+		<tr><td><input type="radio" name="q12" value="4" class="rad"> Henry</td>
+		</tr>
+		<tr><td><input type="radio" name="q12" value="5" class="rad"> Bruce</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q12" value="6" class="rad"> Luke</td>
+		</tr>
+		<tr><th>13. If you had to fight a villain, it would be...</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q13" value="1" class="rad"> The Joker</td>
+		</tr>
+		<tr><td><input type="radio" name="q13" value="2" class="rad"> Doctor Octopus</td>
+		</tr>
+		<tr><td><input type="radio" name="q13" value="3" class="rad"> Darth Vader</td>
+		</tr>
+		<tr><td><input type="radio" name="q13" value="4" class="rad"> Lex Luthor</td>
+		</tr>
+		<tr><td><input type="radio" name="q13" value="5" class="rad"> Professor Moriarty</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q13" value="6" class="rad"> Mr. Potter (Well, that shouldn't be too difficult, seeing how the man is confined to a wheelchair!!)</td>
+		</tr>
+		<tr><th>14. Why do you fight injustice?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q14" value="1" class="rad"> Um...I don't know.</td>
+		</tr>
+		<tr><td><input type="radio" name="q14" value="2" class="rad"> To get rid of injustice, of COURSE!</td>
+		</tr>
+		<tr><td><input type="radio" name="q14" value="3" class="rad"> For truth, justice, and the American way!</td>
+		</tr>
+		<tr><td><input type="radio" name="q14" value="4" class="rad"> For...money?</td>
+		</tr>
+		<tr><td><input type="radio" name="q14" value="5" class="rad"> To keep from getting bored, I guess.</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q14" value="6" class="rad"> Just because that's what I like to do.</td>
+		</tr>
+		<tr><th>15. You received the Call to Adventure!</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q15" value="1" class="rad"> You plug your ears</td>
+		</tr>
+		<tr><td><input type="radio" name="q15" value="2" class="rad"> You say, "Sorry, but I'm busy!"</td>
+		</tr>
+		<tr><td><input type="radio" name="q15" value="3" class="rad"> You accept, rather reluctantly</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q15" value="4" class="rad"> You jump at the call. </td>
+		</tr>
+		<tr><th>16. Are you good-looking?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q16" value="1" class="rad"> Yes, actually.</td>
+		</tr>
+		<tr><td><input type="radio" name="q16" value="2" class="rad"> I don't know.</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q16" value="3" class="rad"> No...</td>
+		</tr>
+		<tr><th>17. Your height?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q17" value="1" class="rad"> Tall</td>
+		</tr>
+		<tr><td><input type="radio" name="q17" value="2" class="rad"> Average</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q17" value="3" class="rad"> Short</td>
+		</tr>
+		<tr><th>18. Are you in high school?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q18" value="1" class="rad"> Yes</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q18" value="2" class="rad"> No</td>
+		</tr>
+		<tr><th>19. Introvert or extrovert?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q19" value="1" class="rad"> Introvert</td>
+		</tr>
+		<tr><td><input type="radio" name="q19" value="2" class="rad"> Extrovert</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q19" value="3" class="rad"> Hm...it varies.</td>
+		</tr>
+		<tr><th>20. Favorite time period?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q20" value="1" class="rad"> Victorian/Edwardian era</td>
+		</tr>
+		<tr><td><input type="radio" name="q20" value="2" class="rad"> The Middle Ages</td>
+		</tr>
+		<tr><td><input type="radio" name="q20" value="3" class="rad"> Modern era</td>
+		</tr>
+		<tr><td><input type="radio" name="q20" value="4" class="rad"> U.S. during the 40's-50's</td>
+		</tr>
+		<tr><td><input type="radio" name="q20" value="5" class="rad"> Sci-fi future era</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q20" value="6" class="rad"> Other</td>
+		</tr>
+		<tr><th>21. Do you have curly hair?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q21" value="1" class="rad"> Yes</td>
+		</tr>
+		<tr><td><input type="radio" name="q21" value="2" class="rad"> No</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q21" value="3" class="rad"> Sort of</td>
+		</tr>
+		<tr><th>22. Can you ride a horse?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q22" value="1" class="rad"> Yes</td>
+		</tr>
+		<tr><td><input type="radio" name="q22" value="2" class="rad"> I prefer a pony</td>
+		</tr>
+		<tr><td><input type="radio" name="q22" value="3" class="rad"> Motorcycles are better</td>
+		</tr>
+		<tr><td><input type="radio" name="q22" value="4" class="rad"> I don't need to. I have a car.</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q22" value="5" class="rad"> No, I can't ride a horse...</td>
+		</tr>
+		<tr><th>23. Are you a parent?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q23" value="1" class="rad"> Yes</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q23" value="2" class="rad"> No</td>
+		</tr>
+		<tr><th>24. Do you have any brothers or sisters?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q24" value="1" class="rad"> Yes</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q24" value="2" class="rad"> No</td>
+		</tr>
+		<tr><th>25. Have you ever yelled, "NOOOOOOO!"?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q25" value="1" class="rad"> Um...yeah</td>
+		</tr>
+		<tr><td><input type="radio" name="q25" value="2" class="rad"> NOOOO!</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q25" value="3" class="rad"> I can't remember</td>
+		</tr>
+		<tr><th>26. How serious are you?</th>
+		</tr>
+		<tr><td class="first"><input type="radio" name="q26" value="1" class="rad"> 1. Dead serious.</td>
+		</tr>
+		<tr><td><input type="radio" name="q26" value="2" class="rad"> 2. Serious</td>
+		</tr>
+		<tr><td><input type="radio" name="q26" value="3" class="rad"> 3. You still see the gravity of a situation, but can take some humor.</td>
+		</tr>
+		<tr><td><input type="radio" name="q26" value="4" class="rad"> 4. You're capable of being serious, but prefer to joke a lot.</td>
+		</tr>
+		<tr><td class="last"><input type="radio" name="q26" value="5" class="rad"> 5. Serious? Ha! Everything's one big joke!</td>
+		</tr>
+		</tbody>
+		</table>
+		<input type="hidden" name="qcount" value="26"><input type="hidden" name="refer" value="https://www.google.com/">        <input type="hidden" name="js" value="0" id="jsField">
+	    <input type="submit" value="Submit Answers" id="btn">
+    </form>
+</body>
+</html>
---
+++ what_fictional_character_are_you/results.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>What Fictional Character Are You?</title>
+	<link type="text/css" rel="stylesheet" href="http://cdn.gotoquiz.com/style/2.0/style.min.css">
+</head>
+<body>
+	<h1>You got: PITBULL</h1>
+	<div>
+		<img src="/what_fictional_character_are_you/pitbull.jpg"/>
+	</div>
+	
+	<p>You're Mr. Worldwide. You play hard, and you work hard. Ultimately, you like the hunt.</p> 
+	<p>When people say you can't, that's when you're really going to try to get out there and get it.</p>
+	<p>Whether you like it or not, you speak your mind. And you're inspiring the next generation to be successful like you.</p>
+	<p>DAAHHHHHHLIN</p>
+	<p><a href="/what_fictional_character_are_you">Play again?</a></p>
+</body>
+</html>
